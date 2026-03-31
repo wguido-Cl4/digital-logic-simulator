1e9b213148edecbdf56f4bef9f7d8c5100c20f52
wguido-Cl4 <wguido@cedarville.edu>
2026-03-31 19:02:21 -0400

    Create Wire.cpp

---
+++ Wire.cpp
@@ -0,0 +1,57 @@
+#include "Wire.h"
+#include <iostream>
+
+using namespace std;
+
+Wire::Wire(int index, const string& name, char value)
+    : index_(index), name_(name), value_(value) {
+}
+
+
+void Wire::setValue(char val) {
+    value = val;
+}
+
+void Wire::setName(const string& nameSet) {
+    name = nameSet;
+}
+
+void Wire::addDrivenGate(Gate* gate) {
+    drives.push_back(gate);
+}
+
+void Wire::appendHistory(char symbol) {
+    history += symbol;
+}
+
+
+char Wire::getValue() const {
+    return value;
+}
+
+string Wire::getName() const {
+    return name;
+}
+
+int Wire::getIndex() const {
+    return index;
+}
+
+const vector<Gate*>& Wire::getDrives() const {
+    return drives;
+}
+
+string Wire::getHistory() const {
+    return history;
+}
+
+
+void Wire::printHistory() const {
+    string label;
+    if (name.empty()) {
+        label = "Wire" + to_string(index);
+    } else {
+        label = name;
+    }
+    cout << label << "\t" << history << endl;
+}
