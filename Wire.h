68166bafa128516027d7e398492c7ad5a9fb2c01
wguido-Cl4 <wguido@cedarville.edu>
2026-03-31 18:42:28 -0400

    Add files via upload

---
+++ Wire.h
@@ -0,0 +1,37 @@
+#ifndef WIRE_H
+#define WIRE_H
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Gate;
+
+class Wire {
+public:
+
+    Wire(int index = 0, const string& name = "", char value = 'X');
+
+    void setValue(char val);
+    void setName(const string& name);
+    void addDrivenGate(Gate* gate);
+    void writeHistory(char symbol);
+
+    char getValue() const;
+    string getName() const;
+    int getIndex() const;
+    const vector<Gate*>& getDrives() const;
+    string getHistory() const;
+
+    void printHistory() const;
+
+private:
+    int            index;
+    string         name;
+    char           value;
+    vector<Gate*>  drives;
+    string         history;
+};
+
+#endif // WIRE_H
